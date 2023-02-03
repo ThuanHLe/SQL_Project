866cdfaec4803819e4d3366091cfc2eeb1c98846
ThuanHLe <huuthuanleus98@gmail.com>
2023-02-02 23:44:33 -0800

    Compare total case and total death
    Total cases with Population
    Highest Infection Rate compared to Population

--- Covid_Portfolio_Project.sql
+++ Covid_Portfolio_Project.sql
@@ -8,3 +8,21 @@ ORDER BY location;
 SELECT *
 FROM Covid19.CovidVaccinations
 ORDER BY date;
+
+-- Comparing Total cases vs. Total deaths
+-- The likelihood of dying when you get infected to Covid-19 in your country
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM Covid19.coviddeaths
+WHERE location LIKE '%state%'
+ORDER BY location;
+
+-- Looking at Total cases with Population
+SELECT location, date, total_cases, population, (total_cases/population)*100 AS InfectionRate
+FROM Covid19.coviddeaths
+ORDER BY location;
+
+-- Looking at country with highest Infection Rate compared to Population
+SELECT location, MAX(total_cases) as HighestInfectionCount, population, MAX(total_cases/population)*100 AS Max_Infectionrate
+FROM Covid19.coviddeaths
+GROUP BY Location, population
+ORDER BY Max_Infectionrate DESC;
